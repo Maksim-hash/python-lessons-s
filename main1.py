c3308c07c62d20fff2536fea2470d0d817b784e9
Maksim <kirilloamaksim1@gmail.com>
2024-08-05 18:12:34 +0300

    1 урок

---
+++ main1.py
@@ -0,0 +1,20 @@
+# print("Hello world")
+# print("Kirillov Maksim")
+# print("Мой возраст:15лет")
+# print((2 + 2 / 4) * 4)
+# print(493 * 4 / 6 + 48)
+# print(((3+2)*4)/((2**2)+3/4))
+# # print(((3-(-3**3))* 4) 
+# print(((3-(-3**2))/(9*3))*4)
+a = " Мой возраст:"
+b = 25
+c = "лет"
+print(a + str(b) + c)  
+a = "10"
+b = 2.0
+print(int(a) + int(b))
+a = "28.5"
+b = 24
+print(float(a) + float(b))
+
+
